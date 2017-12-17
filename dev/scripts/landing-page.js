ea5a0153a5203192b1f8d025519aa1dc7ac972f9
Barnabas Chun <chun.barnabas@gmail.com>
2017-12-17 14:18:16 -0500

    Changed http to https of GET request on categories

--- dev/scripts/landing-page.js
+++ dev/scripts/landing-page.js
@@ -52,7 +52,7 @@ class LandingPage extends React.Component {
         // ajax request for meetup categories
         axios({
             method: 'GET',
-            url: 'http://proxy.hackeryou.com',
+            url: 'https://proxy.hackeryou.com',
             dataResponse: 'json',
             paramsSerializer: function (params) {
                 return Qs.stringify(params, { arrayFormat: 'brackets' })
